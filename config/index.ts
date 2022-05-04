9f7de7ec136413beeb1b84a73bc0b2d3e4a0ba36
Benjo Quilario <benjoquilario@gmail.com>
2022-05-04 19:46:51 +0800

    Add: constant variable that help throughout of all components

---
+++ config/index.ts
@@ -0,0 +1,4 @@
+export const API_URL = `https://api.themoviedb.org/3`;
+export const BASE_URL = 'https://image.tmdb.org/t/p/original';
+export const BACKDROP_SIZE = 'w1280';
+export const POSTER_SIZE = 'w780';
